7a16d4525261a99459714af1ea4100ab52ce039a
Sergei Volkov <sergei.volkov@zalando.de>
2024-12-04 10:04:07 +0100

    apache-airflow: 2.7.3 -> 2.10.3

--- pkgs/by-name/ap/apache-airflow/python-package.nix
+++ pkgs/by-name/ap/apache-airflow/python-package.nix
@@ -19,16 +19,18 @@
 , cryptography
 , deprecated
 , dill
-, flask
-, flask-login
-, flask-appbuilder
-, flask-caching
-, flask-session
-, flask-wtf
+#, flask
+#, flask-login
+#, flask-caching
+#, flask-session
+#, flask-wtf
 , gitpython
 , google-re2
 , graphviz
 , gunicorn
+, hatchling
+, hatch-requirements-txt
+, hatch-fancy-pypi-readme
 , httpx
 , iso8601
 , importlib-resources
@@ -55,6 +57,7 @@
 , pydantic
 , pygments
 , pyjwt
+, pytest-httpbin
 , python-daemon
 , python-dateutil
 , python-nvd3
@@ -86,7 +89,7 @@
 , enabledProviders ? []
 }:
 let
-  version = "2.7.3";
+  version = "2.10.3";
 
   airflow-src = fetchFromGitHub rec {
     owner = "apache";
@@ -95,60 +98,20 @@ let
     # Download using the git protocol rather than using tarballs, because the
     # GitHub archive tarballs don't appear to include tests
     forceFetchGit = true;
-    hash = "sha256-+YbiKFZLigSDbHPaUKIl97kpezW1rIt/j09MMa6lwhQ=";
+    hash = "sha256-R+VsaVnZvgOP+cZUUwaZxrlLqwbabJ882D6o0y5N5vQ=";
   };
-
-  # airflow bundles a web interface, which is built using webpack by an undocumented shell script in airflow's source tree.
-  # This replicates this shell script, fixing bugs in yarn.lock and package.json
-
-  airflow-frontend = mkYarnPackage rec {
-    name = "airflow-frontend";
-
-    src = "${airflow-src}/airflow/www";
-    packageJSON = ./package.json;
-
-    offlineCache = fetchYarnDeps {
-      yarnLock = "${src}/yarn.lock";
-      hash = "sha256-WQKuQgNp35fU6z7owequXOSwoUGJDJYcUgkjPDMOops=";
-    };
-
-    distPhase = "true";
-
-    # The webpack license plugin tries to create /licenses when given the
-    # original relative path
-    postPatch = ''
-      sed -i 's!../../../../licenses/LICENSES-ui.txt!licenses/LICENSES-ui.txt!' webpack.config.js
-    '';
-
-    configurePhase = ''
-      cp -r $node_modules node_modules
-    '';
-
-    buildPhase = ''
-      yarn --offline build
-      find package.json yarn.lock static/css static/js -type f | sort | xargs md5sum > static/dist/sum.md5
-    '';
-
-    installPhase = ''
-      mkdir -p $out/static/
-      cp -r static/dist $out/static
-    '';
-  };
-
-  # Import generated file with metadata for provider dependencies and imports.
-  # Enable additional providers using enabledProviders above.
-  providers = import ./providers.nix;
-  getProviderDeps = provider: map (dep: python.pkgs.${dep}) providers.${provider}.deps;
-  getProviderImports = provider: providers.${provider}.imports;
-  providerDependencies = lib.concatMap getProviderDeps enabledProviders;
-  providerImports = lib.concatMap getProviderImports enabledProviders;
 in
 buildPythonPackage rec {
   pname = "apache-airflow";
   inherit version;
   src = airflow-src;
 
-  disabled = pythonOlder "3.7";
+  pyproject = true;
+  build-system = [
+    hatchling
+  ];
+
+  disabled = pythonOlder "3.8";
 
   propagatedBuildInputs = [
     alembic
@@ -167,12 +130,11 @@ buildPythonPackage rec {
     cryptography
     deprecated
     dill
-    flask
-    flask-appbuilder
-    flask-caching
-    flask-session
-    flask-wtf
-    flask-login
+    #flask
+    #flask-caching
+    #flask-session
+    #flask-wtf
+    #flask-login
     gitpython
     google-re2
     graphviz
@@ -202,6 +164,7 @@ buildPythonPackage rec {
     pydantic
     pygments
     pyjwt
+    pytest-httpbin
     python-daemon
     python-dateutil
     python-nvd3
@@ -222,10 +185,6 @@ buildPythonPackage rec {
     werkzeug
   ] ++ lib.optionals (pythonOlder "3.9") [
     importlib-metadata
-  ] ++ providerDependencies;
-
-  buildInputs = [
-    airflow-frontend
   ];
 
   nativeCheckInputs = [
@@ -240,19 +199,8 @@ buildPythonPackage rec {
   # above
   INSTALL_PROVIDERS_FROM_SOURCES = "true";
 
-  postPatch = ''
-    # https://github.com/apache/airflow/issues/33854
-    substituteInPlace pyproject.toml \
-      --replace '[project]' $'[project]\nname = "apache-airflow"\nversion = "${version}"'
-  '' + lib.optionalString stdenv.hostPlatform.isDarwin ''
-    # Fix failing test on Hydra
-    substituteInPlace airflow/utils/db.py \
-      --replace "/tmp/sqlite_default.db" "$TMPDIR/sqlite_default.db"
-  '';
-
   pythonRelaxDeps = [
     "colorlog"
-    "flask-appbuilder"
     "opentelemetry-api"
     "pathspec"
   ];
@@ -263,14 +211,13 @@ buildPythonPackage rec {
   ];
 
   postInstall = ''
-    cp -rv ${airflow-frontend}/static/dist $out/${python.sitePackages}/airflow/www/static
     # Needed for pythonImportsCheck below
     export HOME=$(mktemp -d)
   '';
 
   pythonImportsCheck = [
     "airflow"
-  ] ++ providerImports;
+  ];
 
   preCheck = ''
     export AIRFLOW_HOME=$HOME
@@ -291,48 +238,10 @@ buildPythonPackage rec {
     "bash_operator_kill" # psutil.AccessDenied
   ];
 
-  # Updates yarn.lock and package.json
-  passthru.updateScript = writeScript "update.sh" ''
-    #!/usr/bin/env nix-shell
-    #!nix-shell -i bash -p common-updater-scripts curl pcre "python3.withPackages (ps: with ps; [ pyyaml ])" yarn2nix
-
-    set -euo pipefail
-
-    # Get new version
-    new_version="$(curl -s https://airflow.apache.org/docs/apache-airflow/stable/release_notes.html |
-      pcregrep -o1 'Airflow ([0-9.]+).' | head -1)"
-    update-source-version ${pname} "$new_version"
-
-    # Update frontend
-    cd ./pkgs/servers/apache-airflow
-    curl -O https://raw.githubusercontent.com/apache/airflow/$new_version/airflow/www/yarn.lock
-    curl -O https://raw.githubusercontent.com/apache/airflow/$new_version/airflow/www/package.json
-    yarn2nix > yarn.nix
-
-    # update provider dependencies
-    ./update-providers.py
-  '';
-
-  # Note on testing the web UI:
-  # You can (manually) test the web UI as follows:
-  #
-  #   nix shell .#apache-airflow
-  #   airflow db reset  # WARNING: this will wipe any existing db state you might have!
-  #   airflow db init
-  #   airflow standalone
-  #
-  # Then navigate to the localhost URL using the credentials printed, try
-  # triggering the 'example_bash_operator' and 'example_bash_operator' DAGs and
-  # see if they report success.
-
   meta = with lib; {
     description = "Programmatically author, schedule and monitor data pipelines";
     homepage = "https://airflow.apache.org/";
     license = licenses.asl20;
     maintainers = with maintainers; [ bhipple gbpdt ingenieroariel ];
-    knownVulnerabilities = [
-      "CVE-2023-50943"
-      "CVE-2023-50944"
-    ];
   };
 }
