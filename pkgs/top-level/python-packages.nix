bb894c9976906a1656ecbaba9861b1647b092b8d
Sergey Volkov <taranarmo@gmail.com>
2025-03-25 17:11:35 +0100

    python3Packages.svcs: init at 25.1.0
    Co-authored-by: Ethan Carter Edwards <ethan@ethancedwards.com>

--- pkgs/top-level/python-packages.nix
+++ pkgs/top-level/python-packages.nix
@@ -16932,6 +16932,8 @@ self: super: with self; {
 
   sv-ttk = callPackage ../development/python-modules/sv-ttk { };
 
+  svcs = callPackage ../development/python-modules/svcs { };
+
   svg-path = callPackage ../development/python-modules/svg-path { };
 
   svg-py = callPackage ../development/python-modules/svg-py { };
